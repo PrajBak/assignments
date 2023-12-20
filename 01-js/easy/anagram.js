25137446f757d60f0c43b3fbf39fe363cfe8fc3f
Prajwal Bakshi <PrajBak>
2023-12-21 03:52:18 +0530

    Solved anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,33 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length !== str2.length) {
+    return false;
+  }
 
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  let charCount = new Map();
+  for(let i = 0; i < str1.length; i++) {
+    if(!charCount.has(str1[i]))
+      charCount.set(str1[i], 1);
+    else
+      charCount.set(str1[i], charCount.get(str1[i]) + 1);
+
+    if(!charCount.has(str2[i])) 
+      charCount.set(str2[i], -1);
+    else 
+      charCount.set(str2[i], charCount.get(str2[i]) - 1);
+  }
+  
+  for(let value of charCount.values()) {
+    if (value !== 0) {
+        return false;
+    }
+  }
+  
+  return true;
 }
 
 module.exports = isAnagram;
